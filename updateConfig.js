030b6bcf0d1777d586a65709cbfbe9ba9a7af9fb
ryan senn <ryan.senn.au@gmail.com>
2020-05-21 13:44:18 +1000

    move to subfolder

--- updateConfig.js
+++ updateConfig.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const contents = fs.readFileSync("config.production.json");
 const config = JSON.parse(contents);
-const externalURL = process.env.url || process.env.RENDER_EXTERNAL_URL;
+const externalURL = process.env.url || process.env.RENDER_EXTERNAL_URL + "/blog";
 if (externalURL) {
   // update the URL in the config
   config.url = externalURL;
